5af50d28978b9108af234d9f6edebc8efae39089
Tim Strijdhorst <tim@decorrespondent.nl>
2020-11-10 17:31:49 +0100

    +use github commandline tool to create PR and link that to the trello card

--- app/commands/CreateCommand.php
+++ app/commands/CreateCommand.php
@@ -25,21 +25,38 @@ class CreateCommand extends Command {
 	}
 	
 	protected function execute(InputInterface $input, OutputInterface $output) {
+		$title         = $input->getArgument('title');
 		$response      = $this->httpClient->post(
 			'https://api.trello.com/1/cards',
 			[
 				'query' => ['key' => $_ENV['TRELLO_API_KEY'], 'token' => $_ENV['TRELLO_API_TOKEN']],
 				'json'  => [
-					'name'      => $input->getArgument('title'),
+					'name'      => $title,
 					'idList'    => $_ENV['TRELLO_LIST_ID_REVIEW'],
 					'idMembers' => $_ENV['TRELLO_MEMBER_ID'],
 				],
 			]
 		);
 		$response      = json_decode((string)$response->getBody(), true, 512, JSON_THROW_ON_ERROR);
-		$trelloCardUrl = $response['shortUrl'];
+		$trelloCardUrl = $response['url'];
+		$trelloCardID  = $response['id'];
 		
-		echo $trelloCardUrl;
+		$output = shell_exec('gh pr create --title '.escapeshellarg($title).' --body '.escapeshellarg($trelloCardUrl));
+		
+		if ($output === null) {
+			throw new \Exception('Something went wrong while trying to make the PR. Output of the command: '.$output);
+		}
+		
+		$githubPRUrl = $output;
+		$this->httpClient->post(
+			"https://api.trello.com/1/cards/{$trelloCardID}/attachments",
+			[
+				'query' => ['key' => $_ENV['TRELLO_API_KEY'], 'token' => $_ENV['TRELLO_API_TOKEN']],
+				'json'  => [
+					'url' => $githubPRUrl,
+				],
+			]
+		);
 		
 		return Command::SUCCESS;
 	}
