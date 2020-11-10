b8dba920d6630c2adccda97672b09ffbb7a98ebb
Tim Strijdhorst <tim@decorrespondent.nl>
2020-11-10 16:21:54 +0100

    +dotenv lib +dotenv config

--- createcard.php
+++ createcard.php
@@ -2,8 +2,11 @@
 
 use createcard\commands\CreateCommand;
 use Symfony\Component\Console\Application;
+use Symfony\Component\Dotenv\Dotenv;
 
 require_once __DIR__.'/vendor/autoload.php';
+
+(new Dotenv())->load(__DIR__.'/.env');
 
 $application = new Application();
 $application->addCommands(
