9663022e3a815de6b105d5482921f8adf8e3d49e
Tim Strijdhorst <tim@decorrespondent.nl>
2020-11-19 16:43:14 +0100

    +better output of github errors

--- app/system/GitHub.php
+++ app/system/GitHub.php
@@ -6,16 +6,16 @@ class GitHub {
 	/**
 	 * @param string $title
 	 * @param string $body
-	 * @return string
+	 * @return string url to the created PR
 	 * @throws \Exception
 	 */
-	public function createPR(string $title, string $body) : string {
-		$githubPRUrl = shell_exec('gh pr create --title '.escapeshellarg($title).' --body '.escapeshellarg($body));
+	public function createPR(string $title, string $body): string {
+		exec('gh pr create --title '.escapeshellarg($title).' --body '.escapeshellarg($body).' 2>&1', $output, $exitCode);
 		
-		if ($githubPRUrl === null) {
-			throw new \Exception('Something went wrong while trying to make the PR. Output of the command: '.$githubPRUrl);
+		if ($exitCode !== 0) {
+			throw new \Exception('Something went wrong while trying to make the PR. Output of the command: '.PHP_EOL.implode(PHP_EOL, $output));
 		}
 		
-		return $githubPRUrl;
+		return $output[0];
 	}
 }
